9388c063ab2e5bc7f7ac7c9ed4aff4a4e9660918
fitzypop <fitzypop@pm.me>
2022-03-03 20:56:03 -0700

    doing crud stuff

--- api/database.py
+++ api/database.py
@@ -20,12 +20,31 @@ async def list_collections() -> list[str]:
     return await database.list_collection_names()
 
 
+async def fetch_exercise(name: str) -> Exercise | None:
+    return await collection.find_one({"name": name})
+
+
 async def fetch_all_exercises() -> list[Exercise]:
     exercises = []
     cursor = collection.find({})
     async for doc in cursor:
         exercises.append(Exercise(**doc))
     return exercises
+
+
+async def create_exercise(exercise: Exercise) -> Exercise:
+    result = await collection.insert_one(exercise)
+    return exercise
+
+
+async def update_exercise(name: str, type: str) -> Exercise:
+    await collection.update_one({"name": name}, {"$set": {"type": type}})
+    return await collection.find_one({"name": name})
+
+
+async def delete_exercise(name: str) -> bool:
+    await collection.delete_one({"name": name})
+    return True
 
 
 def get_app_env() -> str:
--- api/main.py
+++ api/main.py
@@ -1,11 +1,14 @@
-# import json
-# import os
-
-# from bson import json_util
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from starlette.responses import RedirectResponse
 
-from api.database import fetch_all_exercises, get_app_env, list_collections
+from api.database import (
+    create_exercise,
+    fetch_all_exercises,
+    fetch_exercise,
+    get_app_env,
+    list_collections,
+)
+from api.model import Exercise
 
 app = FastAPI()
 
@@ -15,19 +18,40 @@ def read_root():
     return RedirectResponse("/docs")
 
 
-@app.get("/collections")
+@app.get("/collections", response_model=list[str])
 async def get_collections():
     response = await list_collections()
-    return {"collections": response}
+    if response:
+        return response
+    else:
+        raise HTTPException(404, "There are no collections in this database")
 
 
-@app.get("/exercises")
-async def get_exercises():
-    response = await fetch_all_exercises()
+@app.get("/exercise/{name}", response_model=Exercise)
+async def get_exercise(name: str):
+    response = await fetch_exercise(name)
     return response
 
 
-@app.get("/env")
+@app.get("/exercises", response_model=list[Exercise])
+async def get_exercises():
+    response = await fetch_all_exercises()
+    if response:
+        return response
+    else:
+        raise HTTPException(404, "data not found")
+
+
+@app.post("/exercise")
+async def post_exercise(exercise: Exercise):
+    response = await create_exercise(exercise)
+    if response:
+        return response
+    else:
+        raise HTTPException(500)
+
+
+@app.get("/env", response_model=dict[str, str])
 def get_env():
     env = get_app_env()
     return {"app_env": env}
