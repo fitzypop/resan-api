e8c56c55f59c80df735bf8bc98dc21eb8ea7a090
fitzypop <fitzypop@pm.me>
2022-03-12 23:30:57 -0700

    debugging in staging env

--- api/routers/health.py
+++ api/routers/health.py
@@ -1,6 +1,7 @@
 import json
 
 from api.database import client
+from devtools import debug
 from fastapi import APIRouter
 
 router = APIRouter(tags=["health"])
@@ -9,4 +10,5 @@ router = APIRouter(tags=["health"])
 @router.get("/health")
 async def health_check():
     db_results = await client.server_info()
-    return json.dumps(db_results)
+    debug(db_results)
+    return db_results
