d8c6ed5c9207960a3b08d8524fadd4d613ba3c17
fitzypop <fitzypop@pm.me>
2022-03-16 15:46:13 -0600

    finished with settings object

--- api/settings.py
+++ api/settings.py
@@ -24,7 +24,7 @@ class _APISettings:
         class Config:
             env_file = ".env"
 
-        @validator("*")
+        @validator("*", always=True)
         def must_have_value(cls, v):
             if v in [None, ""]:
                 raise ValueError("Env variables not loaded.")
@@ -32,16 +32,29 @@ class _APISettings:
 
     def __init__(self) -> None:
         self._settings = self.Settings()
-        self.db_conn_str = self._settings.db_conn_str.format(
+        self.mongo_conn_str = self._settings.db_conn_str.format(
             self._settings.db_username, self._settings.db_password
         )
         self.db_name = f"Resan{self._settings.app_env}"
 
-    def __getattr__(self, item) -> Any:
-        return getattr(self._settings, item)
+    def __getattr__(self, attr) -> Any:
+        _attr = getattr(self._settings, attr)
+        if hasattr(type(_attr), "__get__"):  # attr is a method
+            _attr = _attr.__get__(
+                self._settings
+            )  # set _settings as self on method call
+        return _attr
+
+    def __delattr__(self, __name: str) -> None:
+        raise NotImplementedError(
+            f"{_APISettings.__name__} does not support deleting attributes."
+        )
 
 
 # Replaced Singleton pattern with lru_cache
+
+# Please either import the function or module variable below.
+# We don't need multiple instances of these settings.
 @lru_cache
 def get_settings() -> _APISettings:
     return api_settings
